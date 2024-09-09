41bcf35f3a18e2d503d12501330d864eb1ddf79c
mk0904 <mk10092004@gmail.com>
2024-09-09 08:39:48 +0530

    completed functions

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-function logInfo(message) {
+const {appendFileSync:afs} = require("fs");
+const path = "./logs/app.log";
 
+function logInfo(message) {
+  afs(path, `\n${new Date().toISOString()} | INFO | ${message}\n`);
 }
 
 function logWarning(message) {
-
+  afs(path, `\n${new Date().toISOString()} | WARNING | ${message}\n`)
 }
 
 function logError(message) {
-
+  afs(path, `\n${new Date().toISOString()} | ERROR | ${message}\n`)
 }
 
 logInfo("This is an info message");
