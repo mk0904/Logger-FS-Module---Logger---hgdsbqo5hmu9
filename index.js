45ea4c9c45fedbdf5032fe4cb9241d87abca9b5b
mk0904 <148270333+mk0904@users.noreply.github.com>
2024-09-09 08:28:39 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+function logInfo(message) {
+
+}
+
+function logWarning(message) {
+
+}
+
+function logError(message) {
+
+}
+
+logInfo("This is an info message");
+logWarning("This is a warning message");
+logError("This is an error message");
+
+module.exports = {
+  logInfo,
+  logWarning,
+  logError,
+};
